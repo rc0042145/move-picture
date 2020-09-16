d541e2cacca2e4cf40d447c935406931edf686db
Wroblewski <RC0042145@student.riversidecollege.ac.uk>
2020-09-16 10:31:46 +0100

    Add project files.

---
+++ move picture/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace move_picture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X + 10, pictureBox2.Location.Y);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 10);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X - 10, pictureBox2.Location.Y);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X + 1000000, pictureBox2.Location.Y);
+        }
+    }
+}
